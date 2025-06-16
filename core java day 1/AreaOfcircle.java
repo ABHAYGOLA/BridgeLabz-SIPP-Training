0f79dd8e94923daaded46626c4d298e8f0d9b8cf
ABHAYGOLA <142886911+ABHAYGOLA@users.noreply.github.com>
2025-06-16 13:13:20 +0530

    Renamed folder from 'core java day 1' to 'Day_1_Practice_Problem'

--- core java day 1/AreaOfcircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class AreaOfcircle{ 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the radius");
-        int a = sc.nextInt();
-        System.out.println("area of circle is :" + (2*3.14*a));
-    }
-}
